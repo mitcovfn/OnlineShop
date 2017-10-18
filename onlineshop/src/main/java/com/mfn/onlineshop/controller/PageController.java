a54731adbaabc66b41284ce848f73055a0c72f96
Fyodor Mitkov <mitcovfn@gmail.com>
2017-10-18 22:57:11 +0300

    Successfully loaded static resources

---
+++ onlineshop/src/main/java/com/mfn/onlineshop/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.mfn.onlineshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		return new ModelAndView("page");
+	}
+}
