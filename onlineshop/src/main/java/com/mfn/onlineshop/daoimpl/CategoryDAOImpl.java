c06e91570097692e2e670a9fb699ff57513f20b7
Fyodor Mitkov <mitcovfn@gmail.com>
2017-10-21 23:45:35 +0300

    Added CRUD operation for Category

--- onlineshop/src/main/java/com/mfn/onlineshop/daoimpl/CategoryDAOImpl.java
+++ onlineshop/src/main/java/com/mfn/onlineshop/daoimpl/CategoryDAOImpl.java
@@ -1,54 +1,71 @@
 package com.mfn.onlineshop.daoimpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mfn.onlineshop.dao.CategoryDAO;
 import com.mfn.onlineshop.entity.Category;
 
 @Repository("categoryDAO")
+@Transactional
 public class CategoryDAOImpl implements CategoryDAO {
 
-	private static List<Category> categories = new ArrayList<>();
-
-	static {
-		Category category = new Category();
-		category.setId(1);
-		category.setName("Mobile");
-		category.setDescription("Mobile description");
-		category.setImageURL("image.png");
-		categories.add(category);
-
-		category = new Category();
-		category.setId(2);
-		category.setName("Television");
-		category.setDescription("Television description");
-		category.setImageURL("image.png");
-		categories.add(category);
-
-		category = new Category();
-		category.setId(3);
-		category.setName("Laptop");
-		category.setDescription("Laptop description");
-		category.setImageURL("image.png");
-		categories.add(category);
-	}
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	@Override
 	public List<Category> list() {
-		return categories;
+
+		String hql = "FROM category WHERE active = :active";
+		Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		query.setParameter("active", true);
+		return query.list();
 	}
 
 	@Override
 	public Category get(int id) {
+		return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
+	}
 
-		for (Category category : categories) {
-			if (category.getId() == id)
-				return category;
+	@Override
+	public boolean add(Category category) {
+		try {
+			sessionFactory.getCurrentSession().persist(category);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
 		}
-		return null;
+	}
+
+	@Override
+	public boolean update(Category category) {
+		try {
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean delete(Category category) {
+
+		category.setActive(false);
+
+		try {
+			sessionFactory.getCurrentSession().update(category);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
--- onlineshop/src/main/java/com/mfn/onlineshop/entity/Category.java
+++ onlineshop/src/main/java/com/mfn/onlineshop/entity/Category.java
@@ -1,11 +1,26 @@
 package com.mfn.onlineshop.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "category")
 public class Category {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String name;
+	
 	private String description;
+	
+	@Column(name = "image_url")
 	private String imageURL;
+	
+	@Column(name = "is_active")
 	private boolean active = true;
 	
 	
@@ -53,5 +68,15 @@ public class Category {
 	public void setActive(boolean active) {
 		this.active = active;
 	}
+
+
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + ", description=" + description + ", imageURL=" + imageURL
+				+ ", active=" + active + "]";
+	}
+	
+	
 	
 }
