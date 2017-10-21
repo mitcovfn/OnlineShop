7e06a36678f4e1e9084b8a8e255f213cbd833d4a
Fyodor Mitkov <mitcovfn@gmail.com>
2017-10-21 23:44:50 +0300

    Configured hibernate and database connection

---
+++ onlineshop/src/main/java/com/mfn/onlineshop/config/HibernateConfig.java
@@ -0,0 +1,61 @@
+package com.mfn.onlineshop.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+
+	private final static String DATABASE_URL = "jdbc:mysql://localhost:3306/onlineshop";
+	private final static String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+	private final static String DATABASE_DIALECT = "org.hibernate.dialect.MySQL5Dialect";
+	private final static String DATABASE_USERNAME = "root";
+	private final static String DATABASE_PASSWORD = "root";
+
+	@Bean
+	public DataSource getDataSource() {
+
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setUrl(DATABASE_URL);
+		dataSource.setDriverClassName(DATABASE_DRIVER);
+		dataSource.setUsername(DATABASE_USERNAME);
+		dataSource.setPassword(DATABASE_PASSWORD);
+
+		return dataSource;
+	}
+
+	@Bean
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
+		builder.addProperties(getHibernateProperties());
+		builder.scanPackages("com.mfn.onlineshop.entity");
+
+		return builder.buildSessionFactory();
+
+	}
+
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", DATABASE_DIALECT);
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+		properties.put("hibernate.hbm2ddl.auto", "update");
+		return properties;
+	}
+
+	@Bean
+	public HibernateTransactionManager geTransactionManager(SessionFactory sessionFactory) {
+		return new HibernateTransactionManager(sessionFactory);
+	}
+
+}
